ad13225166df51fef6dd29dd1b2e66c291d4fa85
Archan26 <archanparmar99@gmail.com>
2019-12-23 20:37:54 +0530

    Basic App with Button

--- app/src/main/java/com/example/monitor/MainActivity.java
+++ app/src/main/java/com/example/monitor/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.monitor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListner);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new VideoFragment()).commit();
+        bottomNav.getMenu().findItem(R.id.nav_video).setChecked(true);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner =
+           new BottomNavigationView.OnNavigationItemSelectedListener() {
+               @Override
+               public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                   Fragment selectedItem = null;
+
+                   switch(menuItem.getItemId()){
+                       case R.id.nav_video:
+                           selectedItem = new VideoFragment();
+                           break;
+                       case R.id.nav_set:
+                           selectedItem = new SetIPFragment();
+                           break;
+                   }
+                   getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                           selectedItem).commit();
+                   return true;
+               }
+           };
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
